149be515113ddc982565dc30359217d85f6c4fbb
Tyler Williamson <tjw0104@gmail.com>
2020-05-17 12:45:14 -0400

    Added option to annotate the class with the JSON Field Name

--- library/src/main/java/com/ominous/tylerutils/util/JsonUtils.java
+++ library/src/main/java/com/ominous/tylerutils/util/JsonUtils.java
@@ -1,9 +1,10 @@
 package com.ominous.tylerutils.util;
 
+import com.ominous.tylerutils.annotation.JSONFieldName;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 
@@ -26,13 +27,16 @@ public class JsonUtils {
         T obj = objClass.newInstance();
 
         for (Field field : objClass.getFields()) {
-            if (json.has(field.getName())) {
+            JSONFieldName fieldAnnotation = field.getAnnotation(JSONFieldName.class);
+            String fieldName = fieldAnnotation == null ? field.getName() : fieldAnnotation.name();
+
+            if (json.has(fieldName)) {
                 if (field.getType().isPrimitive() || field.getType().equals(String.class)) {
-                    field.set(obj, json.get(field.getName()));
+                    field.set(obj, json.get(fieldName));
                 } else if (field.getType().isArray()) {
-                    field.set(obj, deserialize(field.getType().getComponentType(), json.getJSONArray(field.getName())));
+                    field.set(obj, deserialize(field.getType().getComponentType(), json.getJSONArray(fieldName)));
                 } else {
-                    field.set(obj, deserialize(field.getType(), json.getJSONObject(field.getName())));
+                    field.set(obj, deserialize(field.getType(), json.getJSONObject(fieldName)));
                 }
             }
         }
