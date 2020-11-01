478bbf15629d9dae2a4af09e38456eadc78ce844
Tyler Williamson <tjw0104@gmail.com>
2020-11-01 15:08:38 -0500

    Added TODO to fix deprecated methods

--- library/src/main/java/com/ominous/tylerutils/activity/OnboardingActivity.java
+++ library/src/main/java/com/ominous/tylerutils/activity/OnboardingActivity.java
@@ -69,6 +69,7 @@ public abstract class OnboardingActivity extends AppCompatActivity implements Vi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //TODO WindowInsetsController for API 30
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         if (Build.VERSION.SDK_INT >= 26) {
