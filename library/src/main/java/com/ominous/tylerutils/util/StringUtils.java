18f43455dbd0e4ee6083de87b9fbebcacb101f23
Tyler Williamson <tjw0104@gmail.com>
2020-12-06 18:32:48 -0500

    Added StringUtils

---
+++ library/src/main/java/com/ominous/tylerutils/util/StringUtils.java
@@ -0,0 +1,40 @@
+package com.ominous.tylerutils.util;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+    public static String linkify(CharSequence input, Pattern pattern, String defaultScheme) {
+        Matcher matcher = pattern.matcher(input);
+        StringBuffer sb = new StringBuffer(input.length());
+        String scheme, url, link;
+
+        while (matcher.find()) {
+            scheme = matcher.group(1);
+            url = matcher.group(2);
+            link = scheme == null || scheme.isEmpty() ? defaultScheme + "://" + url : matcher.group();
+
+            matcher.appendReplacement(sb,String.format("<a href='%1$s'>%2$s</a>",link,matcher.group()));
+        }
+
+        matcher.appendTail(sb);
+
+        return sb.toString();
+    }
+
+    public static String capitalizeEachWord(String text) {
+        String[] words = text.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0, l = words.length; i < l; i++) {
+            if (i > 0) {
+                result.append(' ');
+            }
+            result.append(Character.toUpperCase(words[i].charAt(0))).append(words[i].substring(1).toLowerCase());
+        }
+
+        return result.toString();
+    }
+}
