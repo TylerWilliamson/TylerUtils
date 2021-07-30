530fb748d7a1ef03ecbdfd00a8c1b47a6cd2cd9d
Tyler Williamson <tjw0104@gmail.com>
2021-07-29 21:17:44 -0400

    Created a Java implementation of the Promise structure

---
+++ library/src/main/java/com/ominous/tylerutils/async/Promise.java
@@ -0,0 +1,154 @@
+/*
+ *     Copyright 2020 - 2021 Tyler Williamson
+ *
+ *     This file is part of TylerUtils.
+ *
+ *     TylerUtils is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     TylerUtils is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with TylerUtils.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.ominous.tylerutils.async;
+
+import java.util.LinkedList;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import androidx.annotation.NonNull;
+
+public class Promise<S,T> {
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+    private final Promise<?,S> parent;
+    private final PromiseCallable<S,T> runCallable;
+    private final PromiseCallable<Throwable,Void> catchCallable;
+    private final LinkedList<Promise<T,?>> nextList = new LinkedList<>();
+    private T result;
+    private Future<T> resultFuture;
+
+    private PromiseState state = PromiseState.NOT_STARTED;
+
+    private Promise(Promise<?,S> parent,@NonNull PromiseCallable<S,T> runCallable, PromiseCallable<Throwable,Void> catchCallable) {
+        this.parent = parent;
+        this.runCallable = runCallable;
+        this.catchCallable = catchCallable;
+
+        start();
+    }
+
+    public static <U> Promise<Void, U> create(final U input) {
+        return create((a) -> input, null);
+    }
+
+    public static <U> Promise<Void, U> create(@NonNull PromiseCallable<Void,U> runCallable) {
+        return create(runCallable, null);
+    }
+
+    public static <U> Promise<Void, U> create(@NonNull PromiseCallable<Void,U> runCallable, PromiseCallable<Throwable,Void> catchCallable) {
+        return new Promise<>(null,runCallable,catchCallable);
+    }
+
+    public <U> Promise<T,U> then(@NonNull PromiseCallable<T,U> runCallable) {
+        return then(runCallable,null);
+    }
+
+    public <U> Promise<T,U> then(@NonNull PromiseCallable<T,U> runCallable, PromiseCallable<Throwable,Void> catchCallable) {
+        Promise<T,U> then = new Promise<>(this, runCallable, catchCallable);
+
+        nextList.add(then);
+
+        return then;
+    }
+
+    private void start() {
+        if (parent == null) {
+            //Then this IS first, and the parent takes a void parameter
+            executor.submit(() -> run(null));
+        } else if (parent.getState() == PromiseState.COMPLETED) {
+            executor.submit(() -> run(parent.getResult()));
+        } else if (parent.getState() == PromiseState.FAILED ||
+                parent.getState() == PromiseState.CANCELLED) {
+            state = PromiseState.CANCELLED;
+        }
+    }
+
+    private void run(S input) {
+        if (state == PromiseState.NOT_STARTED) {
+            state = PromiseState.STARTED;
+
+            resultFuture = executor.submit(() -> runCallable.call(input));
+
+            try {
+                result = resultFuture.get();
+
+                state = PromiseState.COMPLETED;
+            } catch (ExecutionException e) {
+                if (catchCallable != null) {
+                    executor.submit(() -> catchCallable.call(e.getCause()));
+                }
+
+                state = PromiseState.FAILED;
+            } catch (InterruptedException | CancellationException e) {
+                state = PromiseState.CANCELLED;
+            }
+
+            for (Promise<T,?> next : nextList) {
+                if (state == PromiseState.COMPLETED) {
+                    next.run(result);
+                } else {
+                    next.cancel(true);
+                }
+            }
+        }
+    }
+
+    //TODO cancel siblings
+    public void cancel(boolean mayInterruptIfRunning) {
+        if (state == PromiseState.NOT_STARTED || state == PromiseState.STARTED) {
+            state = PromiseState.CANCELLED;
+
+            if (resultFuture != null && mayInterruptIfRunning) {
+                resultFuture.cancel(true);
+            }
+
+            if (parent != null) {
+                parent.cancel(mayInterruptIfRunning);
+            }
+
+            for (Promise<T,?> next : nextList) {
+                next.cancel(mayInterruptIfRunning);
+            }
+        }
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public PromiseState getState() {
+        return state;
+    }
+
+    public enum PromiseState {
+        NOT_STARTED,
+        STARTED,
+        COMPLETED,
+        FAILED,
+        CANCELLED
+    }
+
+    public interface PromiseCallable<S,T> {
+        T call(S input) throws Exception;
+    }
+}
