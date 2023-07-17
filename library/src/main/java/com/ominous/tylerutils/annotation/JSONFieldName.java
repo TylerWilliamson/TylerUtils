538e825115acf2da201a4498ce445b63e10f1bd3
Tyler Williamson <tylerwilliamson51@gmail.com>
2023-07-17 15:22:16 -0400

    Copyright bump

--- library/src/main/java/com/ominous/tylerutils/annotation/JSONFieldName.java
+++ library/src/main/java/com/ominous/tylerutils/annotation/JSONFieldName.java
@@ -1,20 +1,20 @@
 /*
- *     Copyright 2020 - 2022 Tyler Williamson
+ * Copyright 2020 - 2023 Tyler Williamson
  *
- *     This file is part of TylerUtils.
+ * This file is part of TylerUtils.
  *
- *     TylerUtils is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * TylerUtils is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     TylerUtils is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * TylerUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with TylerUtils.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with TylerUtils.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.ominous.tylerutils.annotation;
